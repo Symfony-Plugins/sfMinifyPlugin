fdacd299794ce7a7ed6636990b09961b783392f1
gimler <gimler@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-23 07:41:20 -0500

    sfMinifyPlugin copy files to trunk
    git-svn-id: http://svn.symfony-project.com/plugins/sfMinifyPlugin/trunk@11061 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/filter/sfMinifyFilter.class.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfMinifyFilter automatically adds javascripts and stylesheets information in the sfResponse content.
+ *
+ * @package    symfony
+ * @subpackage sfMinifyPlugin
+ * @author     Gordon Franke
+ * @version    SVN: $Id: sfMinifyFilter.class.php 3244 2007-01-12 14:46:11Z gordon $
+ */
+class sfMinifyFilter extends sfFilter
+{
+  /**
+   * Executes this filter.
+   *
+   * @param sfFilterChain A sfFilterChain instance
+   */
+  public function execute($filterChain)
+  {
+    // execute next filter
+    $filterChain->execute();
+
+    // execute this filter only once
+    $response = $this->getContext()->getResponse();
+
+    // include javascripts and stylesheets
+    $content = $response->getContent();
+    if (false !== ($pos = strpos($content, '</head>')))
+    {
+      sfLoader::loadHelpers(array('Tag', 'Asset', 'SfMinify'));
+      $html = '';
+      if (!$response->getParameter('javascripts_included', false, 'symfony/view/asset'))
+      {
+        $html .= minify_get_javascripts($response, $this->getParameter('javascripts', true));
+      }
+      if (!$response->getParameter('stylesheets_included', false, 'symfony/view/asset'))
+      {
+        $html .= minify_get_stylesheets($response, $this->getParameter('stylesheets', true));
+      }
+
+      if ($html)
+      {
+        $response->setContent(substr($content, 0, $pos).$html.substr($content, $pos));
+      }
+    }
+
+    $response->setParameter('javascripts_included', false, 'symfony/view/asset');
+    $response->setParameter('stylesheets_included', false, 'symfony/view/asset');
+  }
+}
